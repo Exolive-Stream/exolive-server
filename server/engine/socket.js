90282bcc80abd32ff70770fcf0de3db89b435668
scitechiam <scitech.iam@gmail.com>
2024-09-11 01:37:58 -0500

    feat(server) signal socket added

--- server/engine/socket.js
+++ server/engine/socket.js
@@ -1,19 +1,3 @@
 module.exports = (socket) => {
-  // Escuchar la solicitud de unirse a una sala
-  socket.on("joinRoom", (roomId) => {
-    socket.join(roomId);
-    console.log(`${socket.id} se unió a la sala: ${roomId}`);
-
-    // Notificar a otros usuarios en la sala que un nuevo usuario se ha unido
-    socket.to(roomId).emit("user-joined", socket.id);
-  });
-
-  // Manejar señales de WebRTC
-  socket.on("signal", (data) => {
-    // Transmitir la señal a todos los usuarios en la sala excepto al que envió la señal
-    socket.to(data.roomId).emit("signal", {
-      signalData: data.signalData,
-      sender: socket.id,
-    });
-  });
+  require("./socket/signal.js")(socket);
 };
